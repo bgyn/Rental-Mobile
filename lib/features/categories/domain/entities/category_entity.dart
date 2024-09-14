91ad1a1f5275291590e6e237fba046f7795123fc
Bigyan Thanait <bigyanthanait8@gmail.com>
2024-09-14 21:17:06 +0545

    added missing overrride @hascode

--- lib/features/categories/domain/entities/category_entity.dart
+++ lib/features/categories/domain/entities/category_entity.dart
@@ -2,33 +2,37 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'category_entity.g.dart';
 
-
 @JsonSerializable(fieldRename: FieldRename.snake)
-class CategoryEntity{
+class CategoryEntity {
   final int? id;
   final String? categoryName;
-  
-  CategoryEntity(this.id,this.categoryName);
 
-  factory CategoryEntity.fromJson(Map<String,dynamic> json) => _$CategoryEntityFromJson(json);
-  Map<String,dynamic> toJson() => _$CategoryEntityToJson(this);
+  CategoryEntity(this.id, this.categoryName);
 
-  static List<CategoryEntity> fromJsonList(List? json){
-    return json?.map((e)=> CategoryEntity.fromJson(e)).toList() ?? [];
+  factory CategoryEntity.fromJson(Map<String, dynamic> json) =>
+      _$CategoryEntityFromJson(json);
+  Map<String, dynamic> toJson() => _$CategoryEntityToJson(this);
+
+  static List<CategoryEntity> fromJsonList(List? json) {
+    return json?.map((e) => CategoryEntity.fromJson(e)).toList() ?? [];
   }
 
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)){
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType!=runtimeType){
+    if (other.runtimeType != runtimeType) {
       return false;
     }
-    if(other is CategoryEntity){
+    if (other is CategoryEntity) {
       return other.id == id;
     }
     return false;
   }
-  
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ categoryName.hashCode;
+  }
 }
