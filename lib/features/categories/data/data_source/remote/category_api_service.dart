4db93690bdb610d70376b05e7bab079e290791a6
Bigyan Thanait <bigyanthanait8@gmail.com>
2024-09-15 19:50:22 +0545

    Refactor ApiRoutes: use static method for api's

--- lib/features/categories/data/data_source/remote/category_api_service.dart
+++ lib/features/categories/data/data_source/remote/category_api_service.dart
@@ -3,7 +3,7 @@ import 'package:rentpal/config/routes/api_routes.dart';
 
 class CategoryApiService {
   Future<http.Response> getCategoryList() async {
-    final url = ApiRoutes().getcategories();
+    final url = ApiRoutes.getcategories();
     final response = await http.get(Uri.parse(url));
     return response;
   }
