c567865408b25cc5c88840930821c89167ec4f9d
Bigyan Thanait <bigyanthanait8@gmail.com>
2024-10-20 20:51:16 +0545

    feat: added refresh indicator in dashboard page

--- lib/features/home/presentation/pages/dashboard_page.dart
+++ lib/features/home/presentation/pages/dashboard_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rentpal/config/theme/color_palette.dart';
 import 'package:rentpal/features/categories/presentation/bloc/category_list_bloc.dart';
+import 'package:rentpal/features/categories/presentation/bloc/category_list_event.dart';
 import 'package:rentpal/features/categories/presentation/bloc/category_list_state.dart';
 import 'package:rentpal/features/home/presentation/widgets/carousel.dart';
 import 'package:rentpal/features/categories/presentation/widgets/rental_category_option.dart';
@@ -25,83 +26,88 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            systemOverlayStyle: const SystemUiOverlayStyle(
-              statusBarColor: Color.fromARGB(
-                  0, 133, 67, 67), // Set the status bar color to white
-              statusBarIconBrightness: Brightness
-                  .light, // Set icons to dark (visible on white background)
-              statusBarBrightness: Brightness.light, // For iOS status bar
-            ),
-            pinned: true,
-            expandedHeight: 80.0,
-            flexibleSpace: Container(
-              padding: const EdgeInsets.all(15),
-              color: ColorPalette.primaryColor,
-              child: Center(
-                child: SizedBox(
-                  width: double.infinity,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "Search",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
-                        ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        contentPadding:
-                            const EdgeInsets.symmetric(horizontal: 16.0)),
+      body: RefreshIndicator(
+        onRefresh: ()async{
+          context.read<CategoryListBloc>().add(const FetchCategoryList());
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              systemOverlayStyle: const SystemUiOverlayStyle(
+                statusBarColor: Color.fromARGB(
+                    0, 133, 67, 67), // Set the status bar color to white
+                statusBarIconBrightness: Brightness
+                    .light, // Set icons to dark (visible on white background)
+                statusBarBrightness: Brightness.light, // For iOS status bar
+              ),
+              pinned: true,
+              expandedHeight: 80.0,
+              flexibleSpace: Container(
+                padding: const EdgeInsets.all(15),
+                color: ColorPalette.primaryColor,
+                child: Center(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Search",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 16.0)),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const SliverToBoxAdapter(
-            child: RentalCategoryOption(),
-          ),
-          const SliverToBoxAdapter(
-            child: Carousel(),
-          ),
-          BlocBuilder<CategoryListBloc, CategoryListState>(
-            builder: (context, state) {
-              if (state is CategoryListLoading) {
-                return const SliverToBoxAdapter(
-                  child: Center(child: CircularProgressIndicator()),
-                );
-              } else if (state is CategoryListSuccessful) {
-                final categoryList = state.categoryList;
-
-                return SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                      if (index >= categoryList.length) return null;
-
-                      final category = categoryList[index];
-
-                      return BlocProvider(
-                        create: (context) =>
-                            RentitemBloc(sl())..add(FetchRentItem()),
-                        child: RentalCategory(
-                          title: category.categoryName,
-                        ),
-                      );
-                    },
-                    childCount: categoryList!
-                        .length, // Set the child count to the list length
-                  ),
-                );
-              } else {
-                return const SliverToBoxAdapter(
-                  child: Center(child: Text('Unknown state')),
-                );
-              }
-            },
-          )
-          
-        ],
+            const SliverToBoxAdapter(
+              child: RentalCategoryOption(),
+            ),
+            const SliverToBoxAdapter(
+              child: Carousel(),
+            ),
+            BlocBuilder<CategoryListBloc, CategoryListState>(
+              builder: (context, state) {
+                if (state is CategoryListLoading) {
+                  return const SliverToBoxAdapter(
+                    child: Center(child: CircularProgressIndicator()),
+                  );
+                } else if (state is CategoryListSuccessful) {
+                  final categoryList = state.categoryList;
+        
+                  return SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        if (index >= categoryList.length) return null;
+        
+                        final category = categoryList[index];
+        
+                        return BlocProvider(
+                          create: (context) =>
+                              RentitemBloc(sl())..add(FetchRentItem()),
+                          child: RentalCategory(
+                            title: category.categoryName,
+                          ),
+                        );
+                      },
+                      childCount: categoryList!
+                          .length, // Set the child count to the list length
+                    ),
+                  );
+                } else {
+                  return const SliverToBoxAdapter(
+                    child: Center(child: Text('Unknown state')),
+                  );
+                }
+              },
+            )
+            
+          ],
+        ),
       ),
     );
   }
