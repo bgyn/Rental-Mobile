00c6de33874649f4ba4794a66cd001dd499a846b
Bigyan Thanait <bigyanthanait8@gmail.com>
2024-12-17 19:29:03 +0545

    feat: use the shimmer effect instead of circular progress indicator

--- lib/features/home/presentation/pages/dashboard_page.dart
+++ lib/features/home/presentation/pages/dashboard_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rentpal/config/theme/color_palette.dart';
+import 'package:rentpal/core/extension/extension.dart';
 import 'package:rentpal/features/categories/presentation/bloc/category_list_bloc.dart';
 import 'package:rentpal/features/categories/presentation/bloc/category_list_event.dart';
 import 'package:rentpal/features/categories/presentation/bloc/category_list_state.dart';
@@ -11,6 +12,7 @@ import 'package:rentpal/features/rentitem/presentation/bloc/rentitem_bloc.dart';
 import 'package:rentpal/features/rentitem/presentation/bloc/rentitem_event.dart';
 import 'package:rentpal/features/rentitem/presentation/pages/rental_category.dart';
 import 'package:rentpal/injection_container.dart';
+import 'package:shimmer/shimmer.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -27,7 +29,7 @@ class _DashboardPageState extends State<DashboardPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: RefreshIndicator(
-        onRefresh: ()async{
+        onRefresh: () async {
           context.read<CategoryListBloc>().add(const FetchCategoryList());
         },
         child: CustomScrollView(
@@ -73,19 +75,39 @@ class _DashboardPageState extends State<DashboardPage> {
             BlocBuilder<CategoryListBloc, CategoryListState>(
               builder: (context, state) {
                 if (state is CategoryListLoading) {
-                  return const SliverToBoxAdapter(
-                    child: Center(child: CircularProgressIndicator()),
+                  return SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: 0.23.h(context),
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 5,
+                          itemBuilder: (context, state) {
+                            return Shimmer.fromColors(
+                              baseColor: Colors.grey.shade300,
+                              highlightColor: Colors.white,
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 8.0),
+                                width: 0.4.w(context),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey.shade300,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
                   );
                 } else if (state is CategoryListSuccessful) {
                   final categoryList = state.categoryList;
-        
+
                   return SliverList(
                     delegate: SliverChildBuilderDelegate(
                       (BuildContext context, int index) {
                         if (index >= categoryList.length) return null;
-        
+
                         final category = categoryList[index];
-        
+
                         return BlocProvider(
                           create: (context) =>
                               RentitemBloc(sl())..add(FetchRentItem()),
@@ -105,7 +127,6 @@ class _DashboardPageState extends State<DashboardPage> {
                 }
               },
             )
-            
           ],
         ),
       ),
