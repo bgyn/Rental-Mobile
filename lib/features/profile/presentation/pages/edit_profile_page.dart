02c8887e640bfd316db44585537bf066ff7830d7
Bigyan Thanait <bigyanthanait8@gmail.com>
2024-12-20 11:32:32 +0545

    feat: profile update done
    added logic to show the picked image instead of profilePic when the image is edited

--- lib/features/profile/presentation/pages/edit_profile_page.dart
+++ lib/features/profile/presentation/pages/edit_profile_page.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:rentpal/config/theme/color_palette.dart';
 import 'package:rentpal/core/common/add_photo_option.dart';
+import 'package:rentpal/core/constant/url_constant.dart';
 import 'package:rentpal/core/cubit/image_handler_cubit.dart';
 import 'package:rentpal/core/extension/extension.dart';
 import 'package:rentpal/features/profile/presentation/bloc/profile_bloc.dart';
@@ -27,14 +30,16 @@ class _EditProfilePageState extends State<EditProfilePage> {
   final _aboutCtrl = TextEditingController();
   final _dobCtrl = TextEditingController();
   final _key = GlobalKey<FormState>();
+  String? image;
   XFile? _profile;
   String? _gender;
   late ImageHandlerCubit _imageHandlerCubit;
 
   @override
   void initState() {
-    _imageHandlerCubit = context.read<ImageHandlerCubit>();
     super.initState();
+    _fetchProfileData();
+    _imageHandlerCubit = context.read<ImageHandlerCubit>();
   }
 
   @override
@@ -45,6 +50,23 @@ class _EditProfilePageState extends State<EditProfilePage> {
     _phoneCtrl.dispose();
     _dobCtrl.dispose();
     super.dispose();
+  }
+
+  Future<void> _fetchProfileData() async {
+    final profileBloc = context.read<ProfileBloc>();
+    profileBloc.add(ProfileFetch());
+
+    await Future.delayed(Duration.zero);
+    final state = profileBloc.state;
+
+    if (state is ProfileSuccess) {
+      image = state.data.profilePic;
+      _addressCtrl.text = state.data.address ?? '';
+      _phoneCtrl.text = state.data.phone ?? '';
+      _aboutCtrl.text = state.data.aboutYou ?? '';
+      _dobCtrl.text = state.data.dateOfBirth ?? '';
+      _gender = state.data.gender?.capitalize();
+    }
   }
 
   @override
@@ -60,158 +82,192 @@ class _EditProfilePageState extends State<EditProfilePage> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 0.015.toRes(context)),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Form(
-              key: _key,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 0.02.h(context),
-                  ),
-                  Stack(
+      body: BlocBuilder<ProfileBloc, ProfileState>(builder: (context, state) {
+        if (state is ProfileLoading) {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: ColorPalette.primaryColor,
+            ),
+          );
+        }
+        if (state is ProfileSuccess) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 0.015.toRes(context)),
+            child: SafeArea(
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _key,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      BlocListener<ImageHandlerCubit, List<XFile>>(
-                        listener: (context, state) {
-                          if (state.isNotEmpty) {
-                            setState(() {
-                              _profile = state[0];
-                            });
-                          }
-                        },
-                        child: BlocBuilder<ImageHandlerCubit, List<XFile>>(
-                          builder: (context, state) {
-                            return state.isEmpty
-                                ? Container(
-                                    width: 0.4.w(context),
-                                    height: 0.18.h(context),
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      color: Colors.grey.shade200,
-                                    ),
-                                    child: Center(
-                                      child: Icon(
-                                        Icons.person,
-                                        size: 0.1.toRes(context),
-                                        color: Colors.grey.shade600,
-                                      ),
-                                    ),
-                                  )
-                                : ClipRRect(
-                                    borderRadius: BorderRadius.circular(100),
-                                    clipBehavior: Clip.hardEdge,
-                                    child: Container(
-                                      width: 0.4.w(context),
-                                      height: 0.18.h(context),
-                                      decoration: const BoxDecoration(
-                                        shape: BoxShape.circle,
-                                      ),
-                                      child: Image.file(
-                                        File(state[0].path),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  );
-                          },
-                        ),
+                      SizedBox(
+                        height: 0.02.h(context),
                       ),
-                      Positioned(
-                        bottom: 10,
-                        right: 10,
-                        child: GestureDetector(
-                          onTap: () => addPhotoOption(context, "edit"),
-                          child: Icon(
-                            Icons.camera_alt,
-                            size: 0.025.toRes(context),
-                            color: Colors.grey.shade800,
+                      Stack(
+                        children: [
+                          BlocListener<ImageHandlerCubit, List<XFile>>(
+                            listener: (context, state) {
+                              if (state.isNotEmpty) {
+                                setState(() {
+                                  _profile = state[0];
+                                });
+                              }
+                            },
+                            child: BlocBuilder<ImageHandlerCubit, List<XFile>>(
+                              builder: (context, state) {
+                                return state.isEmpty
+                                    ? image == null
+                                        ? const CircleAvatar(
+                                            radius: 50,
+                                            backgroundColor: Colors.grey,
+                                            child: Icon(
+                                              Icons.person,
+                                              size: 50,
+                                              color: Colors.white,
+                                            ),
+                                          )
+                                        : ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(100),
+                                            clipBehavior: Clip.hardEdge,
+                                            child: Container(
+                                              width: 0.4.w(context),
+                                              height: 0.18.h(context),
+                                              decoration: const BoxDecoration(
+                                                shape: BoxShape.circle,
+                                              ),
+                                              child: CachedNetworkImage(
+                                                imageUrl: UrlConstant.mediaUrl +
+                                                    image.toString(),
+                                                fit: BoxFit.cover,
+                                                placeholder: (context, url) =>
+                                                    const CircularProgressIndicator(),
+                                                errorWidget:
+                                                    (context, url, error) =>
+                                                        const Icon(Icons.error),
+                                              ),
+                                            ),
+                                          )
+                                    : ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(100),
+                                        clipBehavior: Clip.hardEdge,
+                                        child: Container(
+                                          width: 0.4.w(context),
+                                          height: 0.18.h(context),
+                                          decoration: const BoxDecoration(
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: Image.file(
+                                            File(state[0].path),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      );
+                              },
+                            ),
                           ),
-                        ),
-                      )
+                          Positioned(
+                            bottom: 10,
+                            right: 10,
+                            child: GestureDetector(
+                              onTap: () => addPhotoOption(context, "edit"),
+                              child: Icon(
+                                Icons.camera_alt,
+                                size: 0.025.toRes(context),
+                                color: ColorPalette.primaryColor,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 0.02.h(context),
+                      ),
+                      ProfileTextFormField(
+                        textEditingController: _dobCtrl,
+                        title: "DOB",
+                        hintText: "yyyy-mm-dd",
+                        readOnly: true,
+                        onTap: () => selectDate(),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Date of birth is required";
+                          }
+                          return null;
+                        },
+                      ),
+                      ProfileDropdownFormField(
+                        title: "Gender",
+                        hintText: "Gender",
+                        items: const [
+                          DropdownMenuItem(value: 'Male', child: Text('Male')),
+                          DropdownMenuItem(
+                              value: 'Female', child: Text('Female')),
+                          DropdownMenuItem(
+                              value: 'Other', child: Text('Other')),
+                        ],
+                        value: _gender,
+                        onChanged: (value) {
+                          setState(() {
+                            _gender = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please select an option';
+                          }
+                          return null;
+                        },
+                      ),
+                      ProfileTextFormField(
+                        textEditingController: _addressCtrl,
+                        title: "City and State",
+                        hintText: "e.g. Bharatpur, Bagmati",
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "City and State is required";
+                          }
+                          return null;
+                        },
+                      ),
+                      ProfileTextFormField(
+                        textEditingController: _phoneCtrl,
+                        title: "Phone Number",
+                        hintText: "9841",
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "The phone number is required";
+                          }
+                          return null;
+                        },
+                      ),
+                      ProfileTextField(
+                        textEditingController: _aboutCtrl,
+                        title: "About you",
+                        hintText: "Tell us something about yourself",
+                      ),
+                      SizedBox(
+                        height: 0.3.h(context),
+                      ),
                     ],
                   ),
-                  SizedBox(
-                    height: 0.02.h(context),
-                  ),
-                  ProfileTextFormField(
-                    textEditingController: _dobCtrl,
-                    title: "DOB",
-                    hintText: "yyyy-mm-dd",
-                    readOnly: true,
-                    onTap: () => selectDate(),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Date of birth is required";
-                      }
-                      return null;
-                    },
-                  ),
-                  ProfileDropdownFormField(
-                    title: "Gender",
-                    hintText: "Gender",
-                    items: const [
-                      DropdownMenuItem(value: 'Option 1', child: Text('Male')),
-                      DropdownMenuItem(
-                          value: 'Option 2', child: Text('Female')),
-                      DropdownMenuItem(value: 'Option 3', child: Text('Other')),
-                    ],
-                    value: _gender,
-                    onChanged: (value) {
-                      setState(() {
-                        _gender = value;
-                      });
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please select an option';
-                      }
-                      return null;
-                    },
-                  ),
-                  ProfileTextFormField(
-                    textEditingController: _addressCtrl,
-                    title: "City and State",
-                    hintText: "e.g. Bharatpur, Bagmati",
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "City and State is required";
-                      }
-                      return null;
-                    },
-                  ),
-                  ProfileTextFormField(
-                    textEditingController: _phoneCtrl,
-                    title: "Phone Number",
-                    hintText: "9841",
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "The phone number is required";
-                      }
-                      return null;
-                    },
-                  ),
-                  ProfileTextField(
-                    textEditingController: _aboutCtrl,
-                    title: "About you",
-                    hintText: "Tell us something about yourself",
-                  ),
-                  SizedBox(
-                    height: 0.3.h(context),
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-      ),
+          );
+        }
+
+        return const SizedBox();
+      }),
       bottomNavigationBar: BlocListener<ProfileBloc, ProfileState>(
         listener: (context, state) {
           if (state is ProfileUpdateSuccess) {
             ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(content: Text('Profile updated successfully!')),
             );
+            context.read<ProfileBloc>().add(ProfileFetch());
+            context.pop();
           }
           if (state is ProfileError) {
             ScaffoldMessenger.of(context).showSnackBar(
